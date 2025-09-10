f21d1ed2e328ab2e8d3480afc18799cc0a776a3f
sshrushti23 <sshrushti098@gmail.com>
2025-09-10 23:42:19 +0530

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,61 @@
+SELECT CAST(Total_Sales AS DECIMAL(10,2)) AS Total_Sales_Millions FROM blinkit_data
+WHERE Outlet_Establishment_Year = 2022;
+
+SELECT CAST(AVG(Total_Sales) AS DECIMAL(10,1)) AS Avg_Sales FROM blinkit_data
+WHERE Outlet_Establishment_Year = 2022;
+
+SELECT COUNT(*) AS No_Of_Items FROM blinkit_data
+WHERE Outlet_Establishment_Year = 2022;
+
+SELECT CAST(AVG(Rating) AS DECIMAL(10,2)) AS Avg_Rating FROM blinkit_data;
+
+SELECT Outlet_Establishment_Year,
+    CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales , 
+	CAST(AVG(Rating) AS DECIMAL(10,2)) AS Avg_Rating,
+	COUNT(*) AS No_Of_Items,
+	CAST(AVG(Total_Sales) AS DECIMAL(10,1)) AS Avg_Sales
+FROM Blinkit_data
+GROUP BY Outlet_Establishment_Year
+ORDER BY Total_Sales DESC;
+
+SELECT Outlet_Location_Type, 
+       ISNULL([Low Fat], 0) AS Low_Fat, 
+       ISNULL([Regular], 0) AS Regular
+FROM 
+(
+    SELECT Outlet_Location_Type, Item_Fat_Content, 
+           CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+    FROM blinkit_data
+    GROUP BY Outlet_Location_Type, Item_Fat_Content
+) AS SourceTable
+PIVOT 
+(
+    SUM(Total_Sales) 
+    FOR Item_Fat_Content IN ([Low Fat], [Regular])
+) AS PivotTable
+ORDER BY Outlet_Location_Type;
+
+SELECT 
+    Outlet_Size, 
+    CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+    CAST((SUM(Total_Sales) * 100.0 / SUM(SUM(Total_Sales)) OVER()) AS DECIMAL(10,2)) AS Sales_Percentage
+FROM blinkit_data
+GROUP BY Outlet_Size
+ORDER BY Total_Sales DESC;
+
+
+SELECT Outlet_Location_Type, CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+FROM blinkit_data
+GROUP BY Outlet_Location_Type
+ORDER BY Total_Sales DESC
+
+SELECT Outlet_Type, 
+CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+		CAST(AVG(Total_Sales) AS DECIMAL(10,0)) AS Avg_Sales,
+		COUNT(*) AS No_Of_Items,
+		CAST(AVG(Rating) AS DECIMAL(10,2)) AS Avg_Rating,
+		CAST(AVG(Item_Visibility) AS DECIMAL(10,2)) AS Item_Visibility
+FROM blinkit_data
+GROUP BY Outlet_Type
+ORDER BY Total_Sales DESC
+
